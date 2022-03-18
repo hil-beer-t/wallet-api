fa419c28a0fbfd0997be79e7ec701b8770cc02cb
hil-beer-t <hil.beer.t.git@gmail.com>
2022-03-17 23:08:03 -0300

    test: find by user email
    This tests if an user found in the H2 database by email is equal to the
    in-memory user

--- src/test/java/com/hilbert/wallet/repository/UserRepositoryTest.java
+++ src/test/java/com/hilbert/wallet/repository/UserRepositoryTest.java
@@ -1,20 +1,42 @@
 package com.hilbert.wallet.repository;
 
 import com.hilbert.wallet.entity.User;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserRepositoryTest {
 
+    private static final String EMAIL = "email@test.com";
+    private static final String PASSWORD = "123";
+
     @Autowired
     UserRepository userRepository;
+
+    @Before
+    public void setup(){
+        User setupUser = new User();
+        setupUser.setName("setup user");
+        setupUser.setPassword(PASSWORD);
+        setupUser.setEmail(EMAIL);
+
+        userRepository.save(setupUser);
+    }
+
+    @After
+    public void tearDown(){
+        userRepository.deleteAll();
+    }
 
     @Test
     public void testSaveUser(){
@@ -26,5 +48,13 @@ public class UserRepositoryTest {
         User savedUser = userRepository.save(unsavedUser);
 
         assertNotNull(savedUser);
+    }
+
+    @Test
+    public void testFindByEmail(){
+        Optional<User> userFound = userRepository.findByEmailEquals(EMAIL);
+
+        assertTrue(userFound.isPresent());
+        assertEquals(userFound.get().getEmail(), EMAIL);
     }
 }
