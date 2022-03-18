afdbbcb9cbfded8e1cbae3790067c1ef1ff99cb6
hil-beer-t <hil.beer.t.git@gmail.com>
2022-03-17 22:08:04 -0300

    first commit

---
+++ src/test/java/com/hilbert/wallet/WalletRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilbert.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
